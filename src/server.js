30161acbba1a563b3a3ec2eb7dd2fca8f5b100b0
URETTS <yurka.b2000@gmail.com>
2025-06-19 22:31:51 +0200

    step 5 left

--- src/server.js
+++ src/server.js
@@ -7,10 +7,19 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import authRouter from './routes/auth.js';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger/swagger.yaml'));
 
 export const startServer = () => {
   const app = express();
@@ -34,6 +43,8 @@ export const startServer = () => {
   app.get('/', (req, res) => {
     res.send('API is working');
   });
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use(notFoundHandler);
 
